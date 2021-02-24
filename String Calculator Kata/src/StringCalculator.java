970bff1b2193af411f233e61e367b0254c47ae10
VaibhavJagtap31 <79515840+VaibhavJagtap31@users.noreply.github.com>
2021-02-24 14:15:10 +0530

    Add files via upload

---
+++ String Calculator Kata/src/StringCalculator.java
@@ -0,0 +1,166 @@
+import java.util.ArrayList;
+
+public class StringCalculator {
+
+    boolean isNegativeFound = false;
+    String errorString = "negatives not allowed - ";
+    public int add(String s) {
+
+        if(s.isEmpty()) {
+            return 0;
+        }
+        else
+        {
+            ArrayList<String> delimiter = findDelimiter(s);
+            s = processString(s);
+            int sum = 0;
+
+            sum = splitAndAdd(delimiter,s);
+
+
+            if(isNegativeFound)
+            {
+
+                throw new UnsupportedOperationException(errorString.substring(0,errorString.length()-2));
+            }
+
+                return sum;
+        }
+
+    }
+
+    private int splitAndAdd(ArrayList<String> delimiter,String s) // splitting the numbers from the string
+    {
+
+        String tempstr = "";
+        int skip = 1,sum = 0;
+        boolean notFound = true;
+        for (int i = 0; i < s.length(); i+=skip) {
+            notFound = true;
+            for(String k: delimiter)
+            {
+                if(i+k.length() <= s.length() && k.compareTo(s.substring(i,i+k.length()))==0)
+                {
+                    skip = k.length();
+                    notFound = false;
+                    break;
+
+                }
+
+            }
+
+            if (notFound) {
+                  skip = 1;
+                  tempstr += s.charAt(i);
+
+            }
+            else {
+
+                sum += ProcessNumberString(tempstr);
+                tempstr = "";
+                }
+        }
+        sum += ProcessNumberString(tempstr);
+
+        return sum;
+    }
+    
+    private int ProcessNumberString (String s)
+    {
+        int sum = 0;
+        String[] innerSplit = s.split("\n");
+        if(innerSplit.length>1)
+        {
+            sum += toNumber(innerSplit[0])+toNumber(innerSplit[1]);
+        }
+        else
+            sum += toNumber(s);
+
+        return sum;
+    }
+    
+//  finding all the delimiters in the String  
+    private ArrayList<String> findDelimiter(String s)           
+    {
+        ArrayList <String> delimiter = new ArrayList<>();
+
+        if(isDelimiterGiven(s))
+        {
+
+            s = s.split("\n")[0];
+            s = s.substring(2);
+            if(s.length()>1)
+            {
+                String tempStr = "";
+                for (int i = 0; i < s.length(); i++) {
+
+                    if(s.charAt(i)=='[')
+                    {
+                        continue;
+                    }
+                    if(s.charAt(i)==']') {
+                        delimiter.add(tempStr);
+                        tempStr = "";
+                    }
+                    else
+                    tempStr += s.charAt(i);
+                }
+            }
+            else
+            {
+                delimiter.add(s);
+            }
+
+
+        }
+        else
+        {
+            delimiter.add(",");
+
+        }
+
+        return delimiter;
+    }
+    
+    private String processString(String s)
+    {
+        if(isDelimiterGiven(s)) {
+
+            int index;
+            for (index = 2; index < s.length(); index++) {
+
+             if(s.charAt(index)=='\n')
+             {
+                 break;
+             }
+            }
+
+            return s.substring(index+1);
+        }
+        return s;
+    }
+    private boolean isDelimiterGiven(String s)
+    {
+        if(s.substring(0,2).compareTo("//")==0)
+        {
+            return  true;
+        }
+        return false;
+
+    }
+    private int toNumber(String s)              // convert a string into number
+    {
+
+           int i = Integer.parseInt(s);
+           if(i<0)
+           {    isNegativeFound = true;
+                errorString += s +", ";
+           }
+          else if(i>1000)
+               i = 0;
+
+           return i;
+
+    }
+
+}
---
+++ String Calculator Kata/src/StringCalculatorTest.java
@@ -0,0 +1,130 @@
+
+import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+
+    StringCalculator calculator = null;
+
+    @Before
+    public void setUp()
+    {
+    	//setting up new calculator after every test case
+        calculator = new StringCalculator(); 
+
+    }
+
+    @After
+    public void destroy()
+    {
+    	//destroy object
+        calculator = null; 
+    }
+
+    @Test
+    public void shouldBe_zero_on_EmptyString()
+    {
+        int expected = 0;
+
+        int result = calculator.add("");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void ShouldBe_the_only_number_in_StringWithonly_one_number()
+    {
+        int expected = 127;
+
+        int result = calculator.add("127");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void ShouldReturn_the_sum_of_two_number_in_a_String()
+    {
+        int expected = 100;
+        int result = calculator.add("35,65");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void SholudReturn_the_sum_of_all_the_numbers_in_a_String()
+    {
+        int expected = 1500;
+
+        int result = calculator.add("500,100,100,100,100,600");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("Number in newline is also considered in this test case")
+    public void ShouldReturn_the_sum_of_numbers_in_newLine_String()
+    {
+        int expected = 6;
+
+        int result = calculator.add("1\n2,3");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void ShouldReturn_the_sum_of_number_separated_by_given_delimiter()
+    {
+        int expected = 24;
+
+        int result = calculator.add("//;\n4;4;4;6;6");
+
+        assertEquals(expected,result);
+
+    }
+
+    @Test(expected = Exception.class)
+    public void ShouldReturn_Exception_message_negative_numbers_in_string()
+    {
+
+            calculator.add("//-\n9-4-4--3");
+
+
+
+    }
+
+    @Test
+    public void ShouldReturn_Sum_of_number_less_than_1000_only_in_string()
+    {
+        int expected = 15;
+
+        int result = calculator.add("///\n2/8/1001/5");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void ShouldReturn_Sum_of_number_with_delimiter_length_more_than_one()
+
+    {
+        int expected = 35;
+
+        int result = calculator.add("//[;;;]\n14;;;12\n9");
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void ShouldReturn_Sum_of_number_with_multiple_delimiter_length_more_than_one()
+    {
+        int expected = 80;
+
+        int result = calculator.add("//[;;;][+++][---][*]\n25+++15*10\n25---5");
+
+        assertEquals(expected,result);
+    }
+
+
+}
+			
